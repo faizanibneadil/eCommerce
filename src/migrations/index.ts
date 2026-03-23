997b9d7ded80114b39a4e8c4dca4c6be6d4ea296
Faizan Adil <faizanibneadil1@google.com>
2026-03-24 02:44:44 +0500

    setup pg

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260323_214259 from './20260323_214259';
+
+export const migrations = [
+  {
+    up: migration_20260323_214259.up,
+    down: migration_20260323_214259.down,
+    name: '20260323_214259'
+  },
+];
